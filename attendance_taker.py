213eef0175ac76204e6f53d75570a8a4962c4f67
Vanbrown99 <vanbrown920@gmail.com>
2025-08-31 17:23:17 +0100

    Add files via upload

--- attendance_taker.py
+++ attendance_taker.py
@@ -58,4 +58,43 @@ class Face_Recognizer:
         self.last_frame_face_centroid_list = []
         self.current_frame_face_centroid_list = []
 
+        # List to save names of objects in frame N-1 and N
+        self.last_frame_face_name_list = []
+        self.current_frame_face_name_list = []
+
+        #  cnt for faces in frame N-1 and N
+        self.last_frame_face_cnt = 0
+        self.current_frame_face_cnt = 0
+
+        # Save the e-distance for faceX when recognizing
+        self.current_frame_face_X_e_distance_list = []
+
+        # Save the positions and names of current faces captured
+        self.current_frame_face_position_list = []
+        #  Save the features of people in current frame
+        self.current_frame_face_feature_list = []
+
+        # e distance between centroid of ROI in last and current frame
+        self.last_current_frame_centroid_e_distance = 0
+
+        #  Reclassify after 'reclassify_interval' frames
+        self.reclassify_interval_cnt = 0
+        self.reclassify_interval = 10
+
+    #  "features_all.csv"  / Get known faces from "features_all.csv"
+    def get_face_database(self):
+        if os.path.exists("data/features_all.csv"):
+            path_features_known_csv = "data/features_all.csv"
+            csv_rd = pd.read_csv(path_features_known_csv, header=None)
+            for i in range(csv_rd.shape[0]):
+                features_someone_arr = []
+                self.face_name_known_list.append(csv_rd.iloc[i][0])
+                for j in range(1, 129):
+                    if csv_rd.iloc[i][j] == '':
+                        features_someone_arr.append('0')
+                    else:
+                        features_someone_arr.append(csv_rd.iloc[i][j])
+                self.face_features_known_list.append(features_someone_arr)
+            logging.info("Faces in Database： %d", len(self.face_features_known_list))
+            return 1
        
