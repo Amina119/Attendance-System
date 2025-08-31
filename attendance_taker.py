d1f8a916e0b6dbecbe57997ade5f8d43c6100dfc
Vanbrown99 <vanbrown920@gmail.com>
2025-08-31 17:16:24 +0100

    Add files via upload

---
+++ attendance_taker.py
@@ -0,0 +1,29 @@
+import dlib
+import numpy as np
+import cv2
+import os
+import pandas as pd
+import time
+import logging
+import sqlite3
+import datetime
+
+
+# Dlib  / Use frontal face detector of Dlib
+detector = dlib.get_frontal_face_detector()
+
+# Dlib landmark / Get face landmarks
+predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+
+# Dlib Resnet Use Dlib resnet50 model to get 128D face descriptor
+face_reco_model = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
+
+# Create a connection to the database
+conn = sqlite3.connect("attendance.db")
+cursor = conn.cursor()
+
+# Create a table for the current date
+current_date = datetime.datetime.now().strftime("%Y_%m_%d")  # Replace hyphens with underscores
+table_name = "attendance" 
+create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (name TEXT, time TEXT, date DATE, UNIQUE(name, date))"
+cursor.execute(create_table_sql)
