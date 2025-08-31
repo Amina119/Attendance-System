840a22c29f79f94b66d6ff67636eb73da95578e6
Vanbrown99 <vanbrown920@gmail.com>
2025-08-31 17:29:51 +0100

    Add files via upload

--- attendance_taker.py
+++ attendance_taker.py
@@ -157,3 +157,19 @@ class Face_Recognizer:
                                  1,
                                  cv2.LINE_AA)
     # insert data in database
+
+    def attendance(self, name):
+        current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+        conn = sqlite3.connect("attendance.db")
+        cursor = conn.cursor()
+        # Check if the name already has an entry for the current date
+        cursor.execute("SELECT * FROM attendance WHERE name = ? AND date = ?", (name, current_date))
+        existing_entry = cursor.fetchone()
+
+        if existing_entry:
+            print(f"{name} is already marked as present for {current_date}")
+        else:
+            current_time = datetime.datetime.now().strftime('%H:%M:%S')
+            cursor.execute("INSERT INTO attendance (name, time, date) VALUES (?, ?, ?)", (name, current_time, current_date))
+            conn.commit()
+            print(f"{name} marked as present for {current_date} at {current_time}")
