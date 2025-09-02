484218a928b0754e7b389c9b35f4e315ec215e1c
Amina Boubakary <boubakaryamina119@gmail.com>
2025-09-02 11:57:25 +0200

    Add files via upload

--- attendance_taker.py
+++ attendance_taker.py
@@ -1,20 +1,18 @@
-import dlib
+import dlib # type: ignore
 import numpy as np
-import cv2
+import cv2 # type: ignore
 import os
-import pandas as pd
+import pandas as pd # type: ignore
 import time
 import logging
 import sqlite3
 import datetime
-
 
 # Dlib  / Use frontal face detector of Dlib
 detector = dlib.get_frontal_face_detector()
 
 # Dlib landmark / Get face landmarks
 predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
-
 # Dlib Resnet Use Dlib resnet50 model to get 128D face descriptor
 face_reco_model = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
 
@@ -23,17 +21,13 @@ conn = sqlite3.connect("attendance.db")
 cursor = conn.cursor()
 
 # Create a table for the current date
-current_date = datetime.datetime.now().strftime("%Y_%m_%d")  # Replace hyphens with underscores
 table_name = "attendance" 
 create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (name TEXT, time TEXT, date DATE, UNIQUE(name, date))"
 cursor.execute(create_table_sql)
 
-
 # Commit changes and close the connection
-
 conn.commit()
 conn.close()
-
 
 class Face_Recognizer:
     def __init__(self):
@@ -156,8 +150,8 @@ class Face_Recognizer:
                                  0.8, (255, 190, 0),
                                  1,
                                  cv2.LINE_AA)
-    # insert data in database
 
+    # insert data in database
     def attendance(self, name):
         current_date = datetime.datetime.now().strftime('%Y-%m-%d')
         conn = sqlite3.connect("attendance.db")
@@ -176,7 +170,7 @@ class Face_Recognizer:
 
         conn.close()
 
-    #  Face detection and recognition wit OT from input video stream
+    #  Face detection and recognition with OT from input video stream
     def process(self, stream):
         # 1.  Get faces known from "features.all.csv"
         if self.get_face_database():
@@ -315,19 +309,11 @@ class Face_Recognizer:
 
                 logging.debug("Frame ends\n\n")
 
-    
-
-
-    def run(self):
-        # cap = cv2.VideoCapture("video.mp4")  # Get video stream from video file
-        cap = cv2.VideoCapture(0)              # Get video stream from camera
-        self.process(cap)
-
-        cap.release()
-        cv2.destroyAllWindows()
-    
-   
-
+def run(self):
+    cap = cv2.VideoCapture(0)              # Get video stream from camera
+    self.process(cap)
+    cap.release()
+    cv2.destroyAllWindows()
 
 def main():
     # logging.basicConfig(level=logging.DEBUG) # Set log level to 'logging.DEBUG' to print debug info of every frame
@@ -335,6 +321,5 @@ def main():
     Face_Recognizer_con = Face_Recognizer()
     Face_Recognizer_con.run()
 
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+     main()
